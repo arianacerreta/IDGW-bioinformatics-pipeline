ff6eb29a0898d1404f30d8ef48a20ceb2c8a2882
Ariana Cerreta <62970197+arianacerreta@users.noreply.github.com>
2025-09-16 09:15:18 -0700

    Create RG_sort_index_fecal.sh

---
+++ project-specific_files/RG_sort_index_fecal.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+#SBATCH -p eight
+#SBATCH -C "ceph"
+#SBATCH -J RG_sort
+#SBATCH --cpus-per-task=32
+#SBATCH --mail-user=email@example.com
+#SBATCH --mail-type=BEGIN,END,FAIL
+
+# Load samtools
+source /usr/modules/init/bash
+module load samtools
+
+# Paths
+OUTDIR="/path/to/output/directory"
+BAMDIR="/path/to/aligned/bams"
+
+TOTAL_THREADS=$SLURM_CPUS_PER_TASK
+JOBS=4
+THREADS_PER_JOB=$(( TOTAL_THREADS / JOBS ))
+export THREADS_PER_JOB OUTDIR
+
+cd "$BAMDIR" || exit 1
+
+ls *.bam | parallel --env THREADS_PER_JOB,OUTDIR -j "$JOBS" "
+    SAMPLE=\$(basename {} .bam)
+    NF=\$(echo \$SAMPLE | awk -F'_' '{print NF}')
+    field_count=\$(echo \"\$SAMPLE\" | awk -F'_' '{print NF}')
+
+    case \$field_count in
+        6)
+            #i023_F05_IDGW_S1_MID1_SAMPLE.bam
+            index=\$(echo \$SAMPLE | cut -d'_' -f1)
+            well=\$(echo \$SAMPLE | cut -d'_' -f2)
+            lib=\$(echo \$SAMPLE | cut -d'_' -f4)
+            sm=\$(echo \$SAMPLE | cut -d'_' -f6)
+            rg=\"@RG\\tID:\${index}_\${well}_\${sm}\\tSM:\${sm}\\tLB:\${lib}\\tPL:ILLUMINA\\tPU:\${index}_\${well}\"
+            ;;
+        7)
+            #i002_B05_IDGW_S14_LOW_1112EK_redo.bam
+            index=\$(echo \$SAMPLE | cut -d'_' -f1)
+            well=\$(echo \$SAMPLE | cut -d'_' -f2)
+            lib=\$(echo \$SAMPLE | cut -d'_' -f4)
+            sm=\$(echo \$SAMPLE | awk -F'_' '{print \$(NF-1)"_"\$NF}')
+            rg=\"@RG\\tID:\${index}_\${well}_\${sm}\\tSM:\${sm}\\tLB:\${lib}\\tPL:ILLUMINA\\tPU:\${index}_\${well}\"
+            ;;
+        *)
+            # all other fields, normal samples
+            #i032_C05_IDGW_S7_HIGH_UI0435_111FR.bam
+            #i036_A06_IDGW_S16_MID1_MID2_1112EQ.bam
+            #i036_A06_IDGW_S16_MID1_MID2_UI0000_99999.bam
+            index=\$(echo \$SAMPLE | cut -d'_' -f1)
+            well=\$(echo \$SAMPLE | cut -d'_' -f2)
+            lib=\$(echo \$SAMPLE | cut -d'_' -f4)
+            sm=\$(echo \$SAMPLE | awk -F'_' '{print \$(NF-2)"_"\$(NF-1)"_"\$NF}')
+            rg=\"@RG\\tID:\${index}_\${well}_\${sm}\\tSM:\${sm}\\tLB:\${lib}\\tPL:ILLUMINA\\tPU:\${index}_\${well}\"
+            ;;
+    esac
+    
+    # Add RG and sort
+    samtools addreplacerg -r \"\$rg\" -O BAM {} \
+      | samtools sort -@ \$THREADS_PER_JOB -o \"\$OUTDIR/\${SAMPLE}_RG_sort.bam\" -
+
+    # Index if missing
+    if [ ! -f \"\$OUTDIR/\${SAMPLE}_RG_sort.bai\" ]; then
+        samtools index \"\$OUTDIR/\${SAMPLE}_RG_sort.bam\"
+    fi
+
+    echo \"Finished processing \$SAMPLE with RG: \$rg\"
+"
