520b4c57707784258548aa5c0ee69f64474fb469
Koryna Boudinot <kboudinot@uidaho.edu>
2025-09-30 09:35:00 -0700

    Update make_fai_dict.sh

--- utility_files/make_fai_dict.sh
+++ utility_files/make_fai_dict.sh
@@ -1,1 +1,18 @@
-Koryna's shell code here
+#!/bin/bash
+#SBATCH -p eight
+#SBATCH -C "ceph"
+#SBATCH --cpus-per-task=1
+#SBATCH -J fai_dict
+#SBATCH --mail-user email@uidaho.edu
+#SBATCH --mail-type=BEGIN,END,FAIL
+
+module load gatk
+module load samtools
+
+cd path/to/reference_genomes/
+
+gatk CreateSequenceDictionary \
+  -R GCA_000002285.4_Dog10K_Boxer_Tasha_genomic.fna \
+  -O GCA_000002285.4_Dog10K_Boxer_Tasha_genomic.dict
+
+samtools faidx /path/to/GCA_000002285.4_Dog10K_Boxer_Tasha_genomic.fna
